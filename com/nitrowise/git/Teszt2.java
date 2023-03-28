a5c0023a11c2501ecfe6ff3392d595f00bb06b18
bodist <tamas.bodis@nitrowise.com>
2023-03-28 15:30:40 +0200

    review

--- com/nitrowise/git/Teszt2.java
+++ com/nitrowise/git/Teszt2.java
@@ -2,6 +2,8 @@ package com.nitrowise.git;
 
 public class Teszt2 {
 
+    static String ALMA = "ALMA";
+
     public static void main(String[] args) {
         System.out.println("asdasdasdasd");
     }
